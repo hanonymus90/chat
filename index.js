1d6c87ce20ac8750bacfed6318bd03b80407c7b2
hanonymus90 <52471384+hanonymus90@users.noreply.github.com>
2021-03-19 18:48:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
